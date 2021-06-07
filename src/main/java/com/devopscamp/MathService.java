02522b0bd02eb706b7ed85da5bd1c8b6afffd4ce
Silvia Dominguez <silvia_dominguezp@hotmail.com>
2021-06-07 17:20:23 +0200

    second commit

---
+++ src/main/java/com/devopscamp/MathService.java
@@ -0,0 +1,12 @@
+package com.devopscamp;
+
+public class MathService {
+		public int multiply(int firstNumber, int secondNumber) {
+
+				return firstNumber * secondNumber;
+		}
+
+		public int subtract(int firstNumber, int secondNumber) {
+				return firstNumber - secondNumber;
+		}
+}
